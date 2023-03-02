3d22477074d4ab21dc1f113534dbbb74b82d3c4c
Jojo <aditya.abdul@weekendinc.com>
2023-03-02 22:42:05 +0700

    adding mobile view works

---
+++ public/icons/chevron-right.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ChevronRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="800"
+      height="800"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="4.819"
+      version="1.1"
+      viewBox="-18.53 -18.53 222.4 222.4"
+      xmlSpace="preserve"
+    >
+      <path
+        fill="currentColor"
+        d="M51.707 185.343a10.692 10.692 0 01-7.593-3.149 10.724 10.724 0 010-15.175l74.352-74.347L44.114 18.32c-4.194-4.194-4.194-10.987 0-15.175 4.194-4.194 10.987-4.194 15.18 0l81.934 81.934c4.194 4.194 4.194 10.987 0 15.175l-81.934 81.939a10.678 10.678 0 01-7.587 3.15z"
+      ></path>
+    </svg>
+  );
+}
+
+export default ChevronRight;
--- src/pages/work/mobile.js
+++ src/pages/work/mobile.js
@@ -1,7 +1,62 @@
+import { Fragment, useEffect, useState } from "react";
+import workList from "@utils/worklist";
+import ChevronRight from "@icons/chevron-right";
+
+const WorkPart = ({ work, open, setOpen }) => {
+  const toggle = () => setOpen(open === work.link ? false : work.link);
+
+  const isOpen = open === work.link;
+
+  return (
+    <div
+      className={`bg-white translate-all duration-200 overflow-hidden ${
+        isOpen ? "h-72" : "h-16"
+      }`}
+    >
+      <button
+        className="flex btn rounded-none btn-lg btn-ghost w-full text-left m-0 outline-none normal-case"
+        onClick={toggle}
+      >
+        <div className="flex-1">{work.name}</div>
+        <div
+          className={`text-primary [&>svg]:w-4 [&>svg]:h-4 transition-all ${
+            isOpen ? "rotate-90" : "rotate-0"
+          }`}
+        >
+          <ChevronRight />
+        </div>
+      </button>
+      <div
+        className={`bg-slate-100 transition-all ${isOpen ? "h-56" : "h-0"}`}
+      ></div>
+    </div>
+  );
+};
+
+const WorkSection = ({ w }) => {
+  const [open, setOpen] = useState(w.works[0].link);
+
+  return (
+    <Fragment key={w.year}>
+      <div className="text-8xl font-extrabold text-primary opacity-30 translate-x-[-25%] translate-y-[15%]">
+        {w.year}
+      </div>
+
+      <div className="relative mb-20 rounded-2xl overflow-hidden">
+        {w.works.map((x) => (
+          <WorkPart key={x.link} work={x} open={open} setOpen={setOpen} />
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
 export default function WorklistMobile() {
   return (
-    <div className="block relative sm:hidden max-w-md px-4 mx-auto border border-red-400">
-      hehehehehe
+    <div className="block relative sm:hidden max-w-md px-4 mx-auto overflow-x-hidden">
+      {workList.map((w) => (
+        <WorkSection key={w.year} w={w} />
+      ))}
     </div>
   );
 }
