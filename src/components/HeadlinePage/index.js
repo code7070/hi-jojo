2748fc5dd6de5f58c12f88f6a3290237e8ba7468
Jojo <aditya.abdul@weekendinc.com>
2023-03-12 00:26:20 +0700

    eslint rules

--- src/components/HeadlinePage/index.js
+++ src/components/HeadlinePage/index.js
@@ -2,10 +2,10 @@ import styles from "@styles/headline-page.module.scss";
 
 export default function HeadlinePage({ line1, line2 }) {
   return (
-    <div className="headerPage mt-20 mb-24 sm:mb-32 text-center px-8">
-      <div className={styles.headline1}>What i do, called</div>
+    <div className="headerPage mt-20 mb-24 px-8 text-center sm:mb-32">
+      <div className={styles.headline1}>{line1}</div>
       <div>
-        <h2 className={styles.headline2}>Works</h2>
+        <h2 className={styles.headline2}>{line2}</h2>
       </div>
     </div>
   );
